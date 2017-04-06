408ba2ed8da5873a76f1193a14309aa2ea38a964
cdore00 <cdore00@yahoo.ca>
2017-04-05 20:45:40 -0400

    Add files via upload

--- servG4.js
+++ servG4.js
@@ -9,7 +9,7 @@ var port = 3000;
 
 var hostname = '';
 var hostURL = '';
-var HOSTclient = 'http://cdore.no-ip.biz';
+var HOSTclient = 'cdore.no-ip.biz';
 
 //var qs = require('querystring');
 const args = process.argv;

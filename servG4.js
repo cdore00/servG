6c390c443c6992b131bebbf73ecf7ce706a7ddb7
cdore00 <cdore00@yahoo.ca>
2017-04-09 13:08:10 -0400

    Add files via upload

--- servG4.js
+++ servG4.js
@@ -12,8 +12,8 @@ var hostURL = '';
 var HOSTclient = 'http://cdore.no-ip.biz/lou/';
 //'cdore00.000webhostapp.com';
 //'http://cdore.no-ip.biz/lou/';
-//https://rawgit.com/cdore00/lou/master/
-//http://192.168.2.10/lou/
+//'https://rawgit.com/cdore00/lou/master/'
+//'http://192.168.2.10/lou/'
 // Pour les mails
 
 //var qs = require('querystring');

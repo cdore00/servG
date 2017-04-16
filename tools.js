e0f84ffafc97d6ee026b61032c3fd9d56327a84e
cdore00 <cdore00@yahoo.ca>
2017-04-16 14:30:37 -0400

    Update tools.js

--- tools.js
+++ tools.js
@@ -3,8 +3,8 @@
 const fs = require('fs');
 const util = require('util');
 var timeZoneAjust = 0;
-//if (process.env.TIME_AJUST)
-//	timeZoneAjust = process.env.TIME_AJUST;
+if (process.env.TIME_AJUST)
+	timeZoneAjust = process.env.TIME_AJUST;
 
 // Return America/New-York Time zone dateTime (yyyy-mm-dd hh:mm:ss)
 exports.getDateTime = function (dateTime){
@@ -14,7 +14,7 @@ exports.getDateTime = function (dateTime){
 	else
 		timeNow = new Date();
 		
-	timeNow.setUTCHours(timeNow.getUTCHours() - timeZoneAjust);
+	timeNow.setUTCHours(timeNow.getUTCHours() + timeZoneAjust);
 	return timeNow.toLocaleString();
 }
 

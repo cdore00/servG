462d4018a147fcfa0510d8c91a94604bb304662a
cdore00 <cdore00@yahoo.ca>
2017-06-02 09:26:49 -0400

    Update servG4.js

--- servG4.js
+++ servG4.js
@@ -22,7 +22,7 @@ if (args[2] && args[2] == 3000){
 	hostURL = 'http://cdore.no-ip.biz/nod/';
 }else{
 	var port = 8080;
-	hostURL = 'https://googserv4-goog-server.1d35.starter-us-east-1.openshiftapps.com/';
+	hostURL = 'https://googserv4-cd-serv.1d35.starter-us-east-1.openshiftapps.com/';
 }
 console.log(hostURL + " args[0]=" + args[0] + " args[1]=" + args[1] + " args[2]=" + args[2]);
 
